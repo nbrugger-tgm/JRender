f5eb63bef3fd8e70b5dfdea921deb4180fe22e04
Nils Brugger <nils.brugger@willhaben.at>
2023-09-18 18:48:46 +0200

    fix: metallic texture

--- src/main/java/com/niton/render/example/Metal.java
+++ src/main/java/com/niton/render/example/Metal.java
@@ -3,6 +3,7 @@ package com.niton.render.example;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.niton.render.material.Material;
+import com.niton.render.material.MaterialFeature;
 import com.niton.render.material.MaterialSurface;
 
 public class Metal extends Material
@@ -12,6 +13,7 @@ public class Metal extends Material
 
 	public Metal(float metallic) {
 		this.metallic = metallic;
+		enableFeature(MaterialFeature.METALLIC, true, "native");
 	}
 	public MaterialSurface getPoint(Vector2 surface2UV) {
 		MaterialSurface sur = new MaterialSurface();
